aaf3d0637f9a4aef6bc3523da4f6d444d2e04b17
Suhas Bhairav <suhasbhairav@gmail.com>
2022-12-17 16:10:36 +0100

    useEffect example

--- tutorials-useeffect/src/App.js
+++ tutorials-useeffect/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
+
+  const [name, setName] = useState("Old Value");
+
+  const changeName = () => {
+    setName("New Value");
+  };
+
+
+  useEffect(() => {
+    console.log("On load...");
+  }, []);
+
+  useEffect(() => {
+    console.log("New Value:", name);
+  }, [name]);
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Name is : {name} <button onClick={changeName}> Change </button>
     </div>
   );
 }
